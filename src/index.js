70af3c5e1e4e8e269c6a442dec1cc94b1092f505
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2021-09-17 17:51:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import "./CSS/styles.scss";
-// import "regenerator-runtime/runtime.js";
 
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
